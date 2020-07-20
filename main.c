0df9044835a5be44a9fe239825f476382691423c
synth9283 <synth9283@gmail.com>
2020-07-20 17:58:12 +1000

    main.c optimization update

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 
-int amount;
+char *buffer;
 clock_t begin;
 void strmnt(char *str);
 
 int main(int argc, char **argv) {
 	begin = clock();
 	for (int i=1;i<argc;i++) {
-		amount = strlen(*(argv+i))-1;
 		strmnt(*(argv+i));
 	}
 	printf("Took: %f seconds\n", (double)(clock()-begin)/CLOCKS_PER_SEC);
@@ -18,18 +18,16 @@ int main(int argc, char **argv) {
 
 void strmnt(char *str) {
 	size_t len = strlen(str);
+	buffer = (char*)malloc(len);
 	for (int i=0;(size_t)i<len;i++) {
 		for (int j=0;j<i+1;j++) {
-			printf("%c", *(str+j));
+			*(buffer+j) = *(str+j);
 		}
-		printf("\n");
+		printf("%s\n", buffer);
 	}
-	for (int i=len;i>0;i--) {
-		for (int j=0;j<amount+1;j++) {
-			if (j==amount) break;
-			else printf("%c", *(str+j));
-		}
-		printf("\n");
-		amount--;
+	for (int i=len-1;i>0;i--) {
+		*(buffer+i) = '\0';
+		printf("%s\n", buffer);
 	}
+	free(buffer);
 }
