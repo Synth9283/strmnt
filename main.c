057c904ec07d8de99ec5d6e539cd16e2d9e1da29
Synth9283 <synth9283@gmail.com>
2020-08-05 22:55:52 +0000

    fixed syntax and removed time.h

--- main.c
+++ main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
 
-clock_t begin;
 void strmnt(char *str);
 
 int main(int argc, char **argv) {
-	begin = clock();
-	for (int i=1;i<argc;i++) {
+	for (int i=1; i<argc; i++) {
 		strmnt(*(argv+i));
 	}
-	printf("Took: %f seconds\n", (double)(clock()-begin)/CLOCKS_PER_SEC);
+
 	return 0;
 }
 
 void strmnt(char *str) {
 	size_t len = strlen(str);
-	for (int i=0;(size_t)i<len;i++) {
+	for (int i=0; (size_t)i<len; i++) {
 		printf("%.*s\n", i, str);
 	}
-	for (int i=len-2;i>0;i--) {
+	for (int i=len-2; i>0; i--) {
 		printf("%.*s\n", i, str);
 	}
 }
+
